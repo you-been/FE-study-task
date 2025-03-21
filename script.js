2c3f30b8f5015acc3e931307e1c8f19f9c897a1e
Youbeen Cho <ybcho97@naver.com>
2025-03-21 18:23:13 +0900

    250321

--- script.js
+++ script.js
@@ -21,11 +21,9 @@ function createMenuHTML(item) {
         <p class="menu-price">${item.price.toLocaleString()}원</p>
         <p class="menu-description">${item.description}</p>
       </div>
-    <li>
-
+    </li>
 	`;
 }
-//질문 왜 매개변수에 typeall이 들어간지
 async function displayMenu( type = 'all' ) {
 	const menuContainer = document.querySelector('.menu-list');
 	const menuItems = await fetchMenuData();
@@ -41,20 +39,20 @@ async function displayMenu( type = 'all' ) {
 			item.type.toLowerCase() === type.toLowerCase())
 	};
 
-}
+	if (filteredItems.length > 0) {
+		let menuHTML = '';
+		filteredItems.forEach(item => {
+			menuHTML += createMenuHTML(item);
+		});
+		menuContainer.innerHTML = menuHTML;
+	}else {
+		menuContainer.innerHTML = '<p class="no-items">해당 카테고리에 메뉴가 없습니다.</p>'
+	}
 
-if (filterdItems.length > 0) {
-	let menuHTML = '';
-	filteredItems.forEach(item => {
-		menuHTML += createMenuHTML(item);
-	});
-	menuContainer.innerHTML = menuHTML;
-}else {
-	menuContainer.innerHTML = '<p class="no-items">해당 카테고리에 메뉴가 없습니다.</p>'
 }
 
 function setupTypeButtons() {
-	const typeButtons = document.querySelectorall('.type-item a');
+	const typeButtons = document.querySelectorAll('.type-item a');
 
 	typeButtons.forEach(button => {
 		button.addEventListener('click', function() {
@@ -63,10 +61,42 @@ function setupTypeButtons() {
 			});
 			this.classList.add('active');
 
-			const menuType = this.textContext;
+			const menuType = this.textContent;
 
 			displayMenu(menuType);
 		})
 	})
 }
+
+// 페이지 로드 시 실행
+document.addEventListener('DOMContentLoaded', () => {
+  // ALL버튼 활성화
+  document.querySelector('.type-item a').classList.add('active');
+
+  // 초기 메뉴 표시 (전체)
+  displayMenu('all');
+
+  // 메뉴 타입 버튼 설정
+  setupTypeButtons();
+});
+
+
+//top button
+const topBtn = document.querySelector('.top-btn');
+
+window.addEventListener('scroll', function() {
+	if(window.scrollY > 200) {
+		topBtn.classList.add('visible');
+	}else {
+		topBtn.classList.remove('visible');
+	}
+});
+
+topBtn.addEventListener('click', function(e) {
+	e.preventDefault();
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	})
+})
 
